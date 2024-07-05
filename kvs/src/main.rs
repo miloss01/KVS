42ee161a08f1ea7b7c60e620d4c26e62834143fe
miloss01 <mikicamiki.bat@gmail.com>
2024-07-05 15:07:21 +0200

    add lru_cache and it's tests

---
+++ kvs/src/main.rs
@@ -0,0 +1,16 @@
+use kvs_lib::LRUCache;
+
+fn main() {
+    let mut lru_cache: LRUCache = LRUCache::new(2);
+
+    // lru_cache.put("key1".to_string(), vec![1, 2, 3]);
+    // lru_cache.put("key2".to_string(), vec![4, 5, 6]);
+
+    // println!("{:?}", lru_cache.get("key1")); // Some([1, 2, 3])
+    // println!("{:?}", lru_cache.get("key3")); // None
+
+    // lru_cache.put("key3".to_string(), vec![7, 8, 9]);
+
+    println!("{:?}", lru_cache.get("key2")); // None (key2 should be evicted)
+    println!("{:?}", lru_cache.get("key3")); // Some([7, 8, 9])
+}
---
+++ kvs/src/structures/lru_cache/test.rs
@@ -0,0 +1,81 @@
+use crate::LRUCache;
+
+#[test]
+fn test_lru_cache_put_and_get() {
+    let mut lru_cache: LRUCache = LRUCache::new(3);
+
+    lru_cache.put("key1", vec![1, 2, 3]);
+    lru_cache.put("key2", vec![4, 5, 6]);
+    lru_cache.put("key3", vec![7, 8, 9]);
+
+    assert_eq!(lru_cache.get("key1"), Some(&vec![1, 2, 3]));
+    assert_eq!(lru_cache.get("key2"), Some(&vec![4, 5, 6]));
+    assert_eq!(lru_cache.get("key3"), Some(&vec![7, 8, 9]));
+}
+
+#[test]
+fn test_eviction() {
+    let mut lru_cache: LRUCache = LRUCache::new(2);
+    lru_cache.put("key1", vec![1, 2, 3]);
+    lru_cache.put("key2", vec![4, 5, 6]);
+    lru_cache.put("key3", vec![7, 8, 9]);
+
+    assert_eq!(lru_cache.get("key1"), None); // key1 should be evicted
+    assert_eq!(lru_cache.get("key2"), Some(&vec![4, 5, 6]));
+    assert_eq!(lru_cache.get("key3"), Some(&vec![7, 8, 9]));
+}
+
+#[test]
+fn test_update_order() {
+    let mut lru_cache: LRUCache = LRUCache::new(2);
+    lru_cache.put("key1", vec![1, 2, 3]);
+    lru_cache.put("key2", vec![4, 5, 6]);
+
+    assert_eq!(lru_cache.get("key1"), Some(&vec![1, 2, 3])); // Access key1 to update order
+    lru_cache.put("key3", vec![7, 8, 9]);
+
+    assert_eq!(lru_cache.get("key2"), None); // key2 should be evicted
+    assert_eq!(lru_cache.get("key1"), Some(&vec![1, 2, 3]));
+    assert_eq!(lru_cache.get("key3"), Some(&vec![7, 8, 9]));
+}
+
+#[test]
+fn test_update_value() {
+    let mut lru_cache: LRUCache = LRUCache::new(2);
+    lru_cache.put("key1", vec![1, 2, 3]);
+    lru_cache.put("key2", vec![4, 5, 6]);
+
+    assert_eq!(lru_cache.get("key1"), Some(&vec![1, 2, 3]));
+
+    lru_cache.put("key1", vec![7, 8, 9]);
+
+    assert_eq!(lru_cache.get("key1"), Some(&vec![7, 8, 9])); // Updated value
+    assert_eq!(lru_cache.get("key2"), Some(&vec![4, 5, 6]));
+}
+
+#[test]
+fn test_access_order() {
+    let mut lru_cache: LRUCache = LRUCache::new(3);
+
+    lru_cache.put("key1", vec![1]);
+    lru_cache.put("key2", vec![2]);
+    lru_cache.put("key3", vec![3]);
+
+    assert_eq!(lru_cache.get_order(), vec!["key1", "key2", "key3"]);
+
+    // Access key2
+    lru_cache.get("key2");
+    assert_eq!(lru_cache.get_order(), vec!["key1", "key3", "key2"]);
+
+    // Access key1
+    lru_cache.get("key1");
+    assert_eq!(lru_cache.get_order(), vec!["key3", "key2", "key1"]);
+
+    // Access key3
+    lru_cache.get("key3");
+    assert_eq!(lru_cache.get_order(), vec!["key2", "key1", "key3"]);
+
+    // Access key2 again
+    lru_cache.get("key2");
+    assert_eq!(lru_cache.get_order(), vec!["key1", "key3", "key2"]);
+}
