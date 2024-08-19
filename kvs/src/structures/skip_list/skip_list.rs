b658345bb724569172c19664e7579bfaba68a0cb
miloss01 <mikicamiki.bat@gmail.com>
2024-08-19 16:26:45 +0200

    add skiplist

--- kvs/src/structures/skip_list/skip_list.rs
+++ kvs/src/structures/skip_list/skip_list.rs
@@ -1,8 +1,9 @@
 use crate::Record;
 use rand::Rng;
+use std::{borrow::BorrowMut, cmp::Ordering};
 
 #[derive(Clone, Debug)]
-struct Node {
+pub struct Node {
     record: Record,
     forward: Vec<Option<Box<Node>>>,
 }
@@ -11,15 +12,15 @@ impl Node {
     fn new(record: Record, level: usize) -> Self {
         Node {
             record,
-            forward: vec![None; level + 1],
+            forward: vec![None; level],
         }
     }
 }
 
 pub struct SkipList {
-    head: Box<Node>,
+    pub head: Box<Node>,
     max_level: usize,
-    level: usize,
+    height: usize,
     probability: f64,
 }
 
@@ -29,7 +30,7 @@ impl SkipList {
         SkipList {
             head: Box::new(Node::new(head_record, max_level)),
             max_level,
-            level: 0,
+            height: 0,
             probability,
         }
     }
@@ -37,14 +38,90 @@ impl SkipList {
     fn random_level(&self) -> usize {
         let mut lvl: usize = 0;
         let mut rng: rand::prelude::ThreadRng = rand::thread_rng();
-        while rng.gen_bool(self.probability) && lvl < self.max_level {
+        while rng.gen_bool(self.probability) && lvl < self.max_level - 1 {
             lvl += 1;
         }
         lvl
     }
 
-    pub fn insert(&mut self, record: Record) {
-        let new_level: usize = self.random_level();
-        let new_node: Node = Node::new(record, new_level + 1);
+    pub fn insert(&mut self, record: &Record) {
+        let new_level = self.random_level();
+        let mut new_node = Node::new(record.clone(), self.max_level);
+
+        for level in 0..self.max_level {
+            let mut current_node = self.head.as_mut();
+            let mut updated = false;
+
+            while let Some(next) = current_node.forward[level].as_deref_mut() {
+                match next.record.key.cmp(&record.key) {
+                    Ordering::Less => {
+                        current_node = current_node.forward[level].as_mut().unwrap();
+                    }
+                    Ordering::Equal => {
+                        next.record = record.clone();
+                        updated = true;
+                        break;
+                    }
+                    Ordering::Greater => break,
+                }
+            }
+
+            if updated {
+                continue;
+            }
+
+            if level <= new_level {
+                if current_node.forward[level].is_none() {
+                    new_node.forward[level] = None;
+                    current_node.forward[level] = Some(Box::new(new_node.clone()));
+                } else {
+                    let next = current_node.forward[level].as_deref().unwrap();
+                    new_node.forward[level] = Some(Box::new(next.clone()));
+                    current_node.forward[level] = Some(Box::new(new_node.clone()));
+                }
+            }
+        }
+    }
+
+    pub fn search(&self, key: Vec<u8>) -> Option<&Record> {
+        for level in (0..self.max_level).rev() {
+            let mut node = &self.head;
+            while let Some(ref next) = node.forward[level] {
+                match next.record.key.cmp(&key) {
+                    Ordering::Less => node = next,
+                    Ordering::Equal => {
+                        println!("na levelu {:?}", level);
+                        return Some(&next.record);
+                    }
+                    Ordering::Greater => break,
+                }
+            }
+        }
+        None
+    }
+
+    pub fn get_all_records(&self) -> Vec<&Record> {
+        let mut records = Vec::new();
+        let mut node = &self.head;
+        while let Some(ref next) = node.forward[0] {
+            records.push(&next.record);
+            node = next;
+        }
+        records
+    }
+
+    pub fn print(&self) {
+        for level in 0..self.max_level {
+            print!("Level {}: ", level);
+            let mut node = &self.head;
+            while let Some(ref next) = node.forward[level] {
+                print!(
+                    "({:?}, {:?}, {}), ",
+                    next.record.key, next.record.value, next.record.tombstone
+                );
+                node = next;
+            }
+            println!();
+        }
     }
 }
--- kvs/src/structures/skip_list/test.rs
+++ kvs/src/structures/skip_list/test.rs
@@ -1,4 +1,130 @@
-use crate::SkipList;
+use crate::{Record, SkipList};
 
 #[test]
-fn proba() {}
+fn proba() {
+    let mut skip_list: SkipList = SkipList::new(10, 0.5);
+
+    let record1: Record = Record::new(vec![1], vec![11], false);
+    let mut record2: Record = Record::new(vec![2], vec![22], false);
+    let record3: Record = Record::new(vec![3], vec![33], false);
+
+    skip_list.insert(&record1);
+    skip_list.insert(&record2);
+    skip_list.insert(&record3);
+
+    println!("{:?}", skip_list.search(vec![1]));
+    println!("{:?}", skip_list.search(vec![2]));
+    println!("{:?}", skip_list.search(vec![3]));
+    println!("{:?}", skip_list.search(vec![4]));
+    println!("{:?}", skip_list.search(vec![5]));
+
+    record2.key = vec![5];
+    skip_list.insert(&record2);
+
+    println!("{:?}", skip_list.head);
+    skip_list.print();
+
+    println!("{:?}", skip_list.search(vec![1]));
+    println!("{:?}", skip_list.search(vec![2]));
+    println!("{:?}", skip_list.search(vec![3]));
+    println!("{:?}", skip_list.search(vec![4]));
+    println!("{:?}", skip_list.search(vec![5]));
+
+    println!("{:?}", skip_list.get_all_records());
+}
+
+#[test]
+fn test_search() {
+    let mut records = Vec::new();
+
+    for i in 1..=10 {
+        let key = vec![i as u8];
+        let value = vec![i as u8 * 11];
+        let record = Record::new(key, value, false);
+        records.push(record);
+    }
+
+    for _i in 1..100 {
+        let mut skip_list: SkipList = SkipList::new(10, 0.5);
+
+        for record in &records {
+            skip_list.insert(record);
+        }
+
+        for i in 1..10 {
+            let record = skip_list.search(records[i].key.clone()).unwrap();
+            assert_eq!(record.key, records[i].key);
+        }
+    }
+}
+
+#[test]
+fn probaaa() {
+    let mut skip_list: SkipList = SkipList::new(5, 0.5);
+    // let mut records = Vec::new();
+
+    for i in 1..=10 {
+        let key = vec![i as u8];
+        let value = vec![i as u8 * 11];
+        let record = Record::new(key, value, false);
+        // records.push(record);
+        skip_list.insert(&record);
+    }
+    skip_list.insert(&Record::new(vec![1 as u8], vec![23 as u8], true));
+    skip_list.insert(&Record::new(vec![5 as u8], vec![75 as u8], false));
+    skip_list.insert(&Record::new(vec![11 as u8], vec![77 as u8], true));
+    // skip_list.insert(&records[0]);
+
+    skip_list.print();
+
+    println!("{:?}", skip_list.search(vec![1 as u8]));
+    println!("{:?}", skip_list.search(vec![5 as u8]));
+    println!("{:?}", skip_list.search(vec![11 as u8]));
+    println!("{:?}", skip_list.search(vec![15 as u8]));
+
+    // println!("{:?}", skip_list.get_all_records());
+}
+
+#[test]
+fn test_update() {
+    let mut records = Vec::new();
+
+    for i in 1..=10 {
+        let key = vec![i as u8];
+        let value = vec![i as u8 * 11];
+        let record = Record::new(key, value, false);
+        records.push(record);
+    }
+
+    for _i in 1..100 {
+        let mut skip_list: SkipList = SkipList::new(10, 0.5);
+
+        for record in &records {
+            skip_list.insert(record);
+        }
+
+        for i in 1..10 {
+            let record = skip_list.search(records[i].key.clone()).unwrap();
+            assert_eq!(record.key, records[i].key);
+            assert_eq!(record.tombstone, records[i].tombstone);
+        }
+
+        records[2].tombstone = true;
+        records[5].tombstone = true;
+        records[8].tombstone = true;
+
+        skip_list.insert(&records[2]);
+        skip_list.insert(&records[5]);
+        skip_list.insert(&records[8]);
+
+        for i in 1..10 {
+            let record = skip_list.search(records[i].key.clone()).unwrap();
+            assert_eq!(record.key, records[i].key);
+            assert_eq!(record.tombstone, records[i].tombstone);
+        }
+
+        assert!(skip_list.search(vec![3]).unwrap().tombstone);
+        assert!(skip_list.search(vec![6]).unwrap().tombstone);
+        assert!(skip_list.search(vec![9]).unwrap().tombstone);
+    }
+}
